6406f5e4393c9003a1508bd08aa8eb628c7338aa
Maham Waseem <mahamw440@gmail.com>
2025-07-19 23:04:57 +0500

    Import files
    Import Footer, Todo, Todos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Headers from "./My_Components/Header";
+import Footer from "./My_Components/Footer";
+import Todo from "./My_Components/TOdo";
+import Todos from "./My_Components/Todos";
 
 function App() {
   return (
     <>
       <Headers/>
+      <Todo/>
       <Todos/>
       <Footer/>
     </>
