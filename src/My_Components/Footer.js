5f5b12868b0ea71b49ee5c01736b36914feb94fc
Maham Waseem <mahamw440@gmail.com>
2025-07-26 21:01:28 +0500

    Update Footer.js

--- src/My_Components/Footer.js
+++ src/My_Components/Footer.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function () {
   return (
-    <div>
-      Footer works!!!
-    </div>
+    <footer className="bg-dark text-light py-3">
+      <p className="text-center"> 
+        Copyright &copy; MyTodoList.com
+      </p>
+      
+    </footer>
   )
 }
