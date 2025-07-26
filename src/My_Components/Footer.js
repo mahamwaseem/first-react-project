8a068160e0f132344a75b284c302adda82030cb3
Maham Waseem <mahamw440@gmail.com>
2025-07-26 21:10:15 +0500

    Style the footer

--- src/My_Components/Footer.js
+++ src/My_Components/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 export default function () {
+  let footerStyle= {
+    position : "absolute",
+    top: "100vh",
+    width: "100%",
+    border: "2px solid red",
+  }
   return (
-    <footer className="bg-dark text-light py-3">
+    <footer className="bg-dark text-light py-3" style={footerStyle}>
       <p className="text-center"> 
         Copyright &copy; MyTodoList.com
       </p>
