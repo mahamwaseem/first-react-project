9f3ed6447692b91244a38076e15601fef736dd59
Maham Waseem <mahamw440@gmail.com>
2025-07-26 20:54:37 +0500

    Update Todos.js

--- src/My_Components/Todos.js
+++ src/My_Components/Todos.js
@@ -6,7 +6,7 @@ Todos(props) {
   return (
     <div className = "container">
       <h3 className = "text-center my-3">Todos List</h3>
-      {props.todos.length===0? "No Todods to display": 
+      {props.todos.length===0? "No Todos to display": 
       props.todos.map((todo) => {
         return  <TodoItem key ={todo.sno} todo={todo} onDelete = {props.onDelete}/>
       })
