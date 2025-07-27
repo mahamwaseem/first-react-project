c7f7f762637aa024979e775169f0998639c691cd
Maham Waseem <mahamw440@gmail.com>
2025-07-27 13:08:22 +0500

    Remove footerStyle function

--- src/My_Components/Footer.js
+++ src/My_Components/Footer.js
@@ -1,14 +1,9 @@
 import React from 'react'
 
 export default function () {
-  let footerStyle= {
-    position : "absolute",
-    top: "100vh",
-    width: "100%",
-    border: "2px solid red",
-  }
+  
   return (
-    <footer className="bg-dark text-light py-3" style={footerStyle}>
+    <footer className="bg-dark text-light py-3" >
       <p className="text-center"> 
         Copyright &copy; MyTodoList.com
       </p>
