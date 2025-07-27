7bc0520b12f690a9e7ca0d7f5dd065ab2e332872
Maham Waseem <mahamw440@gmail.com>
2025-07-27 20:25:00 +0500

    use Effect Hook
    here we store the data in local storage and

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import './App.css';
 import Headers from "./My_Components/Header";
-import  {AddTodo} from './My_Components/AddTodo';
+import { AddTodo } from './My_Components/AddTodo';
 import Footer from "./My_Components/Footer";
 import Todos from "./My_Components/Todos";
-import React,  { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
-  const onDelete = (todo)=>{
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const onDelete = (todo) => {
     console.log("I am ondelete of todo", todo);
 
     setTodos(todos.filter((e) => {
-        return e!==todo;
+      return e !== todo;
     }));
+    //to store data locally
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
 
-  const addTodo = (title, desc) =>{
+
+  const addTodo = (title, desc) => {
     console.log("I am adding this todo", title, desc);
     let sno;
-    if(todos.length==0){
+    if (todos.length == 0) {
       sno = 0;
     }
-    else{
-       sno = todos[todos.length-1].sno + 1;
+    else {
+      sno = todos[todos.length - 1].sno + 1;
     }
-  
-    const myTodo ={
-      sno:sno,
+
+    const myTodo = {
+      sno: sno,
       title: title,
       desc: desc,
     }
@@ -34,31 +45,18 @@ function App() {
     console.log(myTodo);
   }
 
+  const [todos, setTodos] = useState(initTodo);
+  //USEEFFECTHOOK
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
 
-
- const [todos, setTodos] = useState([
-  {
-    sno: 1,
-    title:"Go to the market",
-    desc: "You need to go to the market to get this job done1"
-  },
-  {
-    sno: 2,
-    title:"Go to the mall",
-    desc: "You need to go to the market to get this job done2"
-  },
-  {
-    sno: 3,
-    title:"Go to the masjid",
-    desc: "You need to go to the market to get this job done3"
-  },
-]);
   return (
     <>
-      <Headers title = "My Todos List" searchBar ={true}/>
-      <AddTodo addTodo={addTodo}/>
-      <Todos todos = {todos} onDelete={onDelete}/>
-      <Footer/>
+      <Headers title="My Todos List" searchBar={true} />
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
     </>
   );
 }
--- src/My_Components/AddTodo.js
+++ src/My_Components/AddTodo.js
@@ -4,13 +4,16 @@ export const AddTodo = (props) => {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
 
-  const submit = (e)=>{
+  const submit = (e) => {
     e.preventDefault();
-    if(!title || !desc){
+    if (!title || !desc) {
       alert("Title or Description cannot be blank")
     }
-    props.addTodo(title, desc);
-
+    else {
+      props.addTodo(title, desc);
+      setTitle("");
+      setDesc("");
+    }
   }
   return (
     <div className="container my-3">
@@ -18,12 +21,12 @@ export const AddTodo = (props) => {
       <form onSubmit={submit}>
         <div className="mb-3">
           <label htmlFor="title" className="form-label">Todo Title</label>
-          <input type="text" value={title} onChange={(e) => {setTitle(e.target.value)}} className="form-control" id="title" aria-describedby="emailHelp"/>
-            
+          <input type="text" value={title} onChange={(e) => { setTitle(e.target.value) }} className="form-control" id="title" aria-describedby="emailHelp" />
+
         </div>
         <div className="mb-3">
           <label htmlFor="desc" className="form-label">Todo Description</label>
-          <input type="text" value = {desc} onChange={(e) => {setDesc(e.target.value)}}  className="form-control" id="desc"/>
+          <input type="text" value={desc} onChange={(e) => { setDesc(e.target.value) }} className="form-control" id="desc" />
         </div>
         <button type="submit" className="btn btn-sm btn-success">Add Todo</button>
       </form>
