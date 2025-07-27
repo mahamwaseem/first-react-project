a5eec34b6c2839683186f05e33bdc702d78f44cb
Maham Waseem <mahamw440@gmail.com>
2025-07-27 13:13:38 +0500

    Write the todos in container

--- src/My_Components/Todos.js
+++ src/My_Components/Todos.js
@@ -3,8 +3,12 @@ import { TodoItem} from "../My_Components/TodoItem";
 
 export default function 
 Todos(props) {
+  let myStyle ={
+    minHeight: "70vh",
+    margiin : "10px auto"
+  }
   return (
-    <div className = "container">
+    <div className = "container my-3" style ={myStyle} >
       <h3 className = " my-3">Todos List</h3>
       {props.todos.length===0? "No Todos to display": 
       props.todos.map((todo) => {
